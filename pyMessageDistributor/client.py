7a30127fd94399758301c0b0b591013725dde623
BA-24 <65275759+BA-24@users.noreply.github.com>
2020-07-17 13:30:49 +0200

    improved the code in general
    just a bunch of feature stuff really

--- pyMessageDistributor/client.py
+++ pyMessageDistributor/client.py
@@ -2,13 +2,14 @@ import socket
 import select
 import errno
 import sys
- 
+import time
+
 headerLength = 10
 ip = "127.0.0.1"
-port = 2
- 
-username = input("Username: ")
-channel = input("Channel: ")
+port = 1194
+
+blacklistedUsernames = ["False", "True"] #blacklisted usernames reserved to communicate to the client if the message received was handled correctly
+
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     clientSocket.connect((ip, port))
@@ -16,19 +17,6 @@ except ConnectionRefusedError:
     print('No connection found')
     sys.exit()
 clientSocket.setblocking(False)
- 
-username = username.encode("utf-8")
-usernameHeader = f"{len(username):<{headerLength}}".encode("utf-8")
-channel = channel.encode("utf-8")
-channelHeader = f"{len(channel):<{headerLength}}".encode("utf-8")
-send = usernameHeader + username + channelHeader + channel
-sendHeader = f"{len(send):<{headerLength}}".encode("utf-8")
- 
-clientSocket.send(sendHeader + send)
- 
-testList = [usernameHeader, username, channelHeader, channel]
-for x in testList:
-    print(x.decode("utf-8"))
  
 def rMsg(): #receive message
     try:
@@ -41,36 +29,97 @@ def rMsg(): #receive message
         messageHeader = clientSocket.recv(headerLength)
         messageLength = int(messageHeader.decode("utf-8").strip())
         message = clientSocket.recv(messageLength).decode("utf-8")
- 
+
+        if username in ["False", "True"]:
+            return username, message
         return username, message
     except IOError as e:
         if e.errno == errno.WSAECONNRESET:
-            print("connection closed")
+            print("connection to server closed.")
             sys.exit()
         elif e.errno == errno.EWOULDBLOCK:
             pass
         else:
             return False, e
-def sMsg(msg, channel): #send message
-    try:
-        headerLength = 10
-        msg = msg.encode("utf-8")
-        msgHeader = f"{len(msg) :< {headerLength}}".encode("utf-8")
-        channelHeader = f"{len(channel) :< {headerLength}}".encode("utf-8")
-        send = msgHeader + msg + channelHeader + channel
-        sendHeader = f"{len(send):<{headerLength}}".encode("utf-8")
-        clientSocket.send(sendHeader + send)
-        return send
-    except:
+    except ValueError:
+        print("Message header was not received correctly.")
         return False
+def sMsg(msg, channel): #send message
+    headerLength = 10
+    msg = msg.encode("utf-8")
+    msgHeader = f"{len(msg) :< {headerLength}}".encode("utf-8")
+    channelHeader = f"{len(channel) :< {headerLength}}".encode("utf-8")
+    send = msgHeader + msg + channelHeader + channel
+    sendHeader = f"{len(send):<{headerLength}}".encode("utf-8")
+    clientSocket.send(sendHeader + send)
+    for x in range(10):
+        result = rMsg()
+        if result == None:
+            time.sleep(1)
+        else:
+            break
+    if result == None:
+        print("Server did not accept message.")
+        return None
+    elif result == False:
+        print("Server did not return message received signal. Your message may not have been received.")
+    elif result[0] == "False":
+        print("Message returned False error, reason: " + result[1])
+    elif result[0] == "True":
+        if result[1] == msg.decode("utf-8"):
+            print("Message was sent and received succesfully.")
+        else:
+            print("Message was received incorrectly, message received by server: " + result[1])
    
- 
- 
- 
+
+while True:
+    username = input("Username: ")
+    break
+    if username in blacklistedUsernames:
+        print("Your name is blacklisted. Please pick a different username")
+    else:
+        break
+channel = input("Channel: ")
+print("Connecting to server...")
+username = username.encode("utf-8")
+usernameHeader = f"{len(username):<{headerLength}}".encode("utf-8")
+channel = channel.encode("utf-8")
+channelHeader = f"{len(channel):<{headerLength}}".encode("utf-8")
+send = usernameHeader + username + channelHeader + channel
+sendHeader = f"{len(send):<{headerLength}}".encode("utf-8")
+
+#clientSocket.send("bruh".encode("utf-8"))
+clientSocket.send(sendHeader + send)
+for x in range(10):
+    result = rMsg()
+    if result == None:
+        time.sleep(1)
+    else:
+        break
+if result == None:
+    print("Server did not respond to user info.")
+    sys.exit()
+else:
+    if result[0] == "False":
+        print("Your connection was blocked by the server for the following reason: " + result[1])
+        sys.exit()
+    elif result[0] == username.decode("utf-8") and result[1] == channel.decode("utf-8"):
+        print("Connection to server established")
+    else:
+        print("Your username or channel was not received correctly. Please try re-downloading the program or ask the server host about it.")
+        sys.exit()
+        
+
 while True:
     msg = rMsg()
     if msg:
         print(msg)
+    elif isinstance(msg, list) and msg[0] == "True":
+        pass
+    elif isinstance(msg, list) and msg[0] == "False":
+        print("Unexpected error from server: " + msg[1])
+    elif isinstance(msg, list) and msg[0] == False:
+        print("Unexpected error: ")
+        print(msg[1])
     msg = input("> ")
-    if msg:
-        print("sent message: " + (sMsg(msg, channel)).decode("utf-8"))
+    if msg: sMsg(msg, channel)
